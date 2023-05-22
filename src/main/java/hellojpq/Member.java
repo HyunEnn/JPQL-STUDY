91d277a7cf0ff03431c23302372a6cf9e55d17ca
HYUNJUN, KIM <denny10002@naver.com>
2023-05-22 21:54:20 +0900

    JPQL STUDY

---
+++ src/main/java/hellojpq/Member.java
@@ -0,0 +1,22 @@
+package hellojpq;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne()
+    @JoinColumn(name = "team_id")
+    private Team team;
+}
